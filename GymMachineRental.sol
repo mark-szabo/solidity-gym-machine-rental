588e2c8f6a41fa96ac3bbd4b4cdab71fc03c8dc0
Mark Szabo <3098994+mark-szabo@users.noreply.github.com>
2018-04-29 14:07:12 +0200

    Init smart contract

---
+++ GymMachineRental.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.4.21;
+
+contract GymMachineRental {
+
+    // <contract_variables>
+
+    // </contract_variables>
+
+    function GymMachineRental(uint256 pricePerSlot) public {
+        // TODO
+    }
+
+    function reserve(uint24 slotId) public payable {
+        // TODO
+    }
+
+    function validate(address client) public view returns (bool) {
+        // TODO
+    }
+
+    function withdraw() public {
+        // TODO
+    }
+
+}
